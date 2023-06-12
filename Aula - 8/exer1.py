448893b55218ab2883c72820e2ccf24dbdc32657
Vitor Ruan Firmino de Oliveira <110908588+VitorRuan@users.noreply.github.com>
2023-06-12 08:25:55 -0300

    Update exer1.py

--- Aula - 8/exer1.py
+++ Aula - 8/exer1.py
@@ -4,3 +4,10 @@ sobrenome = input("Entre com seu sobrenome: ")
 nome_completo = nome + ' ' + nome_do_meio + ' ' + sobrenome
 
 print(f"Nome completo: {nome_completo} !")
+
+# Criptografia
+nome_cripto = ''
+for indice in range(0, len(nome_completo)):
+    nome_cripto += chr( ord(nome_completo[indice])+1 )
+
+print(f"Nome Cripto: {nome_cripto}")
